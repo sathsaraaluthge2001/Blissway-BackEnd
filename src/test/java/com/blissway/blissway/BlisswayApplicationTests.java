81bfc36277d279878a78f570e638a8bbfbbc0378
sathsaraaluthge2001 <117668224+sathsaraaluthge2001@users.noreply.github.com>
2025-01-14 14:45:18 +0530

    start

---
+++ src/test/java/com/blissway/blissway/BlisswayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissway.blissway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlisswayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
