ba946d29f330abbdeedfefb6f5d8df05a3fa0776
Ramith Kalhara <kalhara7ramith17@gmail.com>
2025-01-19 18:13:36 +0530

    voo

--- src/main/java/com/blissway/blissway/entity/Blog.java
+++ src/main/java/com/blissway/blissway/entity/Blog.java
@@ -15,52 +15,44 @@ import java.util.List;
 @Entity
 public class Blog {
 
+ 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "f_name")
-    private String fName;
+    private String title;
 
-    @Column(name = "l_name")
-    private String lName;
+    @Column(length = 5000)
+    private String paragraph1;
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String title;
-//
-//    @Column(length = 5000)
-//    private String paragraph1;
-//
-//    private String image;
-//
-//    @Embedded
-//    private Author author;
-//
-//    @ElementCollection
-//    private List<String> tags;
-//
-//    private String publishDate;
-//
-//    @Column(length = 5000)
-//    private String paragraph2;
-//
-//    @Column(length = 5000)
-//    private String paragraph3;
-//
-//    @Embedded
-//    private MoreLink moreLink;
-//
-//    private String miniTitle;
-//
-//    @Column(length = 5000)
-//    private String paragraph4;
-//
-//    @ElementCollection
-//    private List<String> listUl;
-//
-//    @Column(length = 5000)
-//    private String paragraph5;
+    private String image;
+
+    @Embedded
+    private Author author;
+
+    @ElementCollection
+    private List<String> tags;
+
+    private String publishDate;
+
+    @Column(length = 5000)
+    private String paragraph2;
+
+    @Column(length = 5000)
+    private String paragraph3;
+
+    @Embedded
+    private MoreLink moreLink;
+
+    private String miniTitle;
+
+    @Column(length = 5000)
+    private String paragraph4;
+
+    @ElementCollection
+    private List<String> listUl;
+
+    @Column(length = 5000)
+    private String paragraph5;
 }
